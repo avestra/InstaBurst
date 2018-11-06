0b50775300d1f2e42ce2fe596fb29cda921b59ce
Mohamed <Ethical.Hacker720@gmail.com>
2018-11-06 08:42:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # configure the repository file
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
 # update outdated archive-key
 wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
